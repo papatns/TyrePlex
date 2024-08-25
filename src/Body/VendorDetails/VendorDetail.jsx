f768f9f14b00015b471e0a4e6b06aac3495bf7ec
papatns <hirishabhshukla@gmail.com>
2024-08-25 23:58:26 +0530

    message 4

--- src/Body/VendorDetails/VendorDetail.jsx
+++ src/Body/VendorDetails/VendorDetail.jsx
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
 import { MdLocationOn } from "react-icons/md";
 import { IoIosTime } from "react-icons/io";
 import Pic1 from "../../assets/pic1.jpg";
