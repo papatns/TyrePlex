9ccdfbd13fe713fd33afba931210712dc8a4cd6b
papatns <hirishabhshukla@gmail.com>
2024-08-26 23:29:35 +0530

    commit 14

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from "./Header/Header"
 import Body from "./Body/Body"
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
